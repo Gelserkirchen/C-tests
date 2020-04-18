5494cd13b534d01c58205f44632fce82f3f61440
gelserkirchen@Mail.ru <Tatevik1993$>
2020-04-18 10:58:42 +0300

    Initial Commit

---
+++ ShildtBook/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ShildtBook
+//
+//  Created by gelserkirchen on 18.04.2020.
+//  Copyright © 2020 gelserkirchen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
